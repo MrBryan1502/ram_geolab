b4bc082f87266955149f407cb6682f093603b26a
BryanM <bryanm@infoexpo.com.mx>
2024-11-19 22:11:29 -0600

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import styles from './page.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
